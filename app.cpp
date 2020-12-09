f17fe205f2af23f4c571835c3eea97dbf185d362
pytrader1x <willukmail@gmail.com>
2020-12-09 18:33:58 +0000

    updated app.cpp with new class instances

--- app.cpp
+++ app.cpp
@@ -11,9 +11,18 @@ int main() {
   sam.add_hobby("Tennis");
   sam.add_hobby("c++");
   sam.add_hobby("Python");
+  
+  Profile george("George the_quant", 28, "Tokyo", "Japan", "he/him");
 
+  george.add_hobby("Rugby");
+  george.add_hobby("R coding language");
+  george.add_hobby("C++ for trading");
+  george.add_hobby("Baking cakes");
 
   std::cout << sam.view_profile();
+
+
+  std::cout << george.view_profile();
 
 
 
